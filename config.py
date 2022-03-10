b3375191b87be4a0c983736167afe23e82c0caa5
dashney <daniel.ashney@portlandoregon.gov>
2022-03-10 14:09:49 -0800

    repointed to new WO layer that has active and closed combined - actually pointing at view on my copy which gives max date (most recent)- once Andrew sets this up as a table point to that and won't need QL anymore

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ projection_file = r"\\besfile1\ASM_AssetMgmt\Projects\Lateral_Failure_Cohorts\pr
 
 master_output_gdb = r"\\besfile1\ASM_AssetMgmt\Projects\Lateral_Failure_Cohorts\master_lateral.gdb"
 
-connections = r"\\oberon\grp117\DAshney\Scripts\connections"
+connections = r"\\besfile1\grp117\DAshney\Scripts\connections"
 
 EGH_PUBLIC = os.path.join(connections, "egh_public on gisdb1.rose.portland.local.sde")
 BESGEORPT_TEST = os.path.join(connections, "BESDBTEST1.BESGEORPT.OSA.sde")
@@ -21,14 +21,12 @@ collection_lines = EGH_PUBLIC + r"\EGH_Public.ARCMAP_ADMIN.collection_lines_bes_
 taxlots = EGH_PUBLIC + r"\EGH_PUBLIC.ARCMAP_ADMIN.taxlots_pdx"
 tv_obs_lines = EGH_PUBLIC + r"\EGH_PUBLIC.ARCMAP_ADMIN.collection_tvobs_line_bes_pdx"
 pdx_boundary = EGH_PUBLIC + r"\EGH_PUBLIC.ARCMAP_ADMIN.portland_pdx"
-WOs = EGH_PUBLIC + r"\EGH_PUBLIC.ARCMAP_ADMIN.COLLECTION_TAXLOTS_BES_ALL_WOS"
+WOs_all = EGH_PUBLIC + r"\EGH_PUBLIC.ARCMAP_ADMIN.COLLECTION_TAXLOTS_BES_ALL_WOS" # uncertain how this is used yet
 
 print "MASTER LATERAL PREP -    Subsetting inputs: " + datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-# move WOs source to PROD - revise WO views to combine into one or get an alternate created for this - include missing fields that are in Hansen but not in mapped - verify with Joe/Grant that query is what we want
-#WOs = arcpy.MakeQueryLayer_management(BESGEORPT_TEST, "WOs",
-#                                      "SELECT OID, DCA_ID, OWNER1, SITEADDR, PROPERTYID, TLID, COMPKEY, MODBY, DISTRICT, WO, ACTCODE, UNITID, UNITID2, INITDTTM, STARTDTTM,"
-#                                      " SCHEDDTTM, PRI, PROB, COMPFLAG, STRUCTURAL_RATING, STRUCTURALSCORE, SHAPE_STArea__, SHAPE_STLength__, STATUS, Shape "
-#                                      "from BESGEORPT.GIS.V_WOS_COMBINED_COPY20211006 WHERE ACTCODE in ('LNRLAT','R/RLAT')", "OID")
+
+# this will become a standalone table which will no longer require the make query layer
+WOs = arcpy.MakeQueryLayer_management(BESGEORPT_TEST, "WOs", "SELECT * from BESGEORPT.GIS.v_WOs_max_INITDTTM", "OBJECTID")
 
 mains = arcpy.MakeFeatureLayer_management(collection_lines, r"in_memory\pipes", "LAYER_GROUP not in ('LATERALS', 'INLETS') AND SERVSTAT not in ('ABAN', 'TBAB' ) AND SYMBOL_GROUP not like 'ABANDONED%' AND LAYER_GROUP = 'SEWER PIPES'")
 laterals = arcpy.MakeFeatureLayer_management(collection_lines, r"in_memory\laterals", "LAYER_GROUP = 'LATERALS' AND SERVSTAT not in ('ABAN', 'TBAB' ) AND SYMBOL_GROUP not like 'ABANDONED%'")
@@ -40,7 +38,7 @@ arcpy.SelectLayerByLocation_management(WOs, "HAVE_THEIR_CENTER_IN", pdx_boundary
 arcpy.SelectLayerByLocation_management(taxlots_layer, "HAVE_THEIR_CENTER_IN", pdx_boundary)
 
 print "MASTER LATERAL PREP -    Reading features into memory: " + datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-WOs_copy = arcpy.CopyFeatures_management(WOs, r"in_memory\WOs_copy")
+WOs_copy = arcpy.CopyFeatures_management(WOs, r"in_memory\WOs_copy") # can add DCA_ID here if still needed
 mains_copy = arcpy.CopyFeatures_management(mains, r"in_memory\mains_copy")
 laterals_copy = arcpy.CopyFeatures_management(laterals, r"in_memory\laterals_copy")
 taxlots_copy = arcpy.CopyFeatures_management(taxlots_layer, r"in_memory\taxlots_copy")
@@ -48,10 +46,11 @@ roots_copy = arcpy.CopyFeatures_management(roots, r"in_memory\roots_copy")
 
 mains_field_list = ['UNITID', 'COMPKEY', 'GLOBALID', 'SERVSTAT', 'FRM_DEPTH', 'TO_DEPTH', 'PIPESIZE', 'MATERIAL', 'JobNo', 'Install_Date', 'LAYER_GROUP', 'SYMBOL_GROUP', 'DETAIL_SYMBOL']
 laterals_field_list = ['UNITID', 'COMPKEY', 'GLOBALID', 'OWNRSHIP', 'SERVSTAT', 'SRVY_LEN', 'PIPESIZE', 'MATERIAL', 'JobNo', 'DATA_SRC', 'Install_Date', 'LAYER_GROUP', 'SYMBOL_GROUP', 'DETAIL_SYMBOL', 'Address', 'Lateral_Depth']
-WOs_field_list = ['DCA_ID', 'WO', 'COMPKEY', 'PROPERTYID', 'STATUS', 'ACTCODE', 'PROB', 'INITDTTM', 'STARTDTTM', 'SCHEDDTTM']
+WOs_field_list = ['WO', 'COMPKEY', 'PROPERTYID', 'COMPFLAG', 'ACTCODE', 'PROB', 'INITDTTM', 'STARTDTTM', 'SCHEDDTTM', 'COMPDTTM', 'ACTUALPROB', 'FINDING', 'MAINLINE_COMPKEY', 'WO_COMMENTS']
 taxlot_field_list = ['PROPERTYID', 'YEARBUILT','LANDUSE', 'SITEADDR']
 TV_obs_field_list = ['COMPKEY', 'OBDEGREE', 'OBRATING']
 
+print "MASTER LATERAL PREP -    Preparing fields: " + datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 utility.prepare_fields(mains_copy, mains_field_list, "Main_")
 print utility.get_field_names(mains_copy)
 print arcpy.GetCount_management(mains_copy)
